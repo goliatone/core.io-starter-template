e937c9fbed93b8a95ec85f52d15e54d8efa9b954
goliatone <burgosemi@gmail.com>
2017-06-05 13:22:50 -0400

    Add persistence optional files

---
+++ optional-files/persistence/templates/src/config/persistence.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+    moduleName: 'persistence',
+    seedDB: true,
+    timeout: 30 * 1000,
+    orm: {
+        adapters: {
+            'disk': require('sails-disk')
+        },
+        connections: {
+            development: {
+                adapter: 'disk'
+            },
+            staging: {
+                adapter: 'disk'
+            }
+        },
+        defaults: {
+            migrate: process.env.NODE_ENV === 'production' ? 'safe' : 'drop',
+            connection: process.env.NODE_PERSISTENCE_CONNECTION || 'production'
+        }
+    }
+};
