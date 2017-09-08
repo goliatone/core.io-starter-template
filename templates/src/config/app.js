3120a7eb7964c2bec4b4523a5351563ffa4774df
goliatone <burgosemi@gmail.com>
2017-09-08 18:18:41 -0400

    Fix config app

--- templates/src/config/app.js
+++ templates/src/config/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /*
- * http://patorjk.com/software/taag/#p=display&h=2&v=2&c=bash&w=.&f=ANSI%20Shadow&t=phonebooth-dashboard
+ * http://patorjk.com/software/taag/#p=display&h=2&v=2&c=bash&w=.&f=ANSI%20Shadow&t=%{name}%
  */
 const banner = require('fs').readFileSync('./config/app.banner.txt', 'utf-8');
 const resolve = require('path').resolve;
