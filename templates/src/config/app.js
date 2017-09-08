f6310a7d0834865b3fec9aa311a5184683cc07ca
goliatone <burgosemi@gmail.com>
2017-09-08 18:04:47 -0400

    Fix config app use resolve

--- templates/src/config/app.js
+++ templates/src/config/app.js
@@ -1,13 +1,14 @@
 'use strict';
 
 /*
- * http://patorjk.com/software/taag/#p=display&h=2&v=2&c=bash&w=.&f=ANSI%20Shadow&t=%{name}%
+ * http://patorjk.com/software/taag/#p=display&h=2&v=2&c=bash&w=.&f=ANSI%20Shadow&t=phonebooth-dashboard
  */
 const banner = require('fs').readFileSync('./config/app.banner.txt', 'utf-8');
+const resolve = require('path').resolve;
 
 module.exports = {
     banner,
     name: '%{name}%',
-    basepath: __dirname,
+    basepath: resolve('./'),
     environment: process.env.NODE_ENV || 'development',
 };
