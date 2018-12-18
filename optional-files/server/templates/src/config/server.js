c4ac1a9895d39749f112daa47d2cbab0630adb1f
goliatone <burgosemi@gmail.com>
2018-12-18 21:18:26 +0100

    add dependents on server

--- optional-files/server/templates/src/config/server.js
+++ optional-files/server/templates/src/config/server.js
@@ -1,5 +1,10 @@
 'use strict';
 
 module.exports = {
-    port: process.env.NODE_APP_PORT || 7331 
+    port: process.env.NODE_APP_PORT || 7331,
+    dependencies: [
+        {%- if dependents.server -%}
+        {% for dep in dependents.server -%}'%{dep}%',{%- endfor %}
+        {%- endif -%}
+    ]
 };
